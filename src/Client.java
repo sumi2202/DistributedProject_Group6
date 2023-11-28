b9383b6dc89e576b74f50147fae9a6c661a6980d
laurynerobi <71785767+laurynerobi@users.noreply.github.com>
2023-11-28 05:21:38 -0500

    Added a close bracket

--- src/Client.java
+++ src/Client.java
@@ -16,36 +16,36 @@ public class Client {
             subscriptionSender.connect("tcp://localhost:5559");
 
             // Scanner for user input
-            Scanner scanner = new Scanner(System.in);
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (true) {
+                    // Authenticate user
+                    System.out.println("Hello, Welcome to FoodPlug, your number 1 food ordering service:");
+                    System.out.println("Enter customer ID:");
+                    String customerId = scanner.nextLine();
+                    System.out.println("Enter first name:");
+                    String firstName = scanner.nextLine();
+                    System.out.println("Enter last name:");
+                    String lastName = scanner.nextLine();
+                    System.out.println("Enter email:");
+                    String email = scanner.nextLine();
 
-            while (true) {
-                // Authenticate user
-                System.out.println("Hello, Welcome to FoodPlug, your number 1 food ordering service:");
-                System.out.println("Enter customer ID:");
-                String customerId = scanner.nextLine();
-                System.out.println("Enter first name:");
-                String firstName = scanner.nextLine();
-                System.out.println("Enter last name:");
-                String lastName = scanner.nextLine();
-                System.out.println("Enter email:");
-                String email = scanner.nextLine();
+                    // authenticating the user based on the database
+                    Database database = new Database();
+                    boolean isAuthenticated = database.authenticate(customerId, firstName, lastName, email);
+                    if (!isAuthenticated) {
+                        System.out.println("Authentication failed");
+                        continue;
+                    }
 
-                // authenticating the user based on the database
-                Database database = new Database();
-                boolean isAuthenticated = database.authenticate(customerId, firstName, lastName, email);
-                if (!isAuthenticated) {
-                    System.out.println("Authentication failed");
-                    continue;
+                    // Send subscription preferences to the Restaurant Server
+                    System.out.println("Enter restaurants to subscribe to (comma-separated):");
+                    String[] restaurants = scanner.nextLine().split(",");
+                    subscriptionSender.send(String.join(",", restaurants));
+
+                    // Accessing messages from the Updates Server
+                    String receivedUpdates = clientSubscriber.recvStr(0);
+                    System.out.println("Received: " + receivedUpdates);
                 }
-
-                // Send subscription preferences to the Restaurant Server
-                System.out.println("Enter restaurants to subscribe to (comma-separated):");
-                String[] restaurants = scanner.nextLine().split(",");
-                subscriptionSender.send(String.join(",", restaurants));
-
-                // Accessing messages from the Updates Server
-                String receivedUpdates = clientSubscriber.recvStr(0);
-                System.out.println("Received: " + receivedUpdates);
             }
         }
     }
